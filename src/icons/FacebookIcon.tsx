646e977f8bd71f0d9cdbd738975a702804409165
Abinesh <108071239+knightempire@users.noreply.github.com>
2025-08-04 16:39:23 +0530

    Add FacebookIcon SVG component

---
+++ src/icons/FacebookIcon.tsx
@@ -0,0 +1,9 @@
+// FacebookIcon.tsx
+export default function FacebookIcon(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg width={20} height={20} viewBox="0 0 20 20" fill="none" {...props}>
+      <rect width="20" height="20" rx="4" fill="#1877F3"/>
+      <path d="M13.5 10.5h-2v6h-2v-6H8V9h1.5V8c0-.83.67-1.5 1.5-1.5h2v2h-2v1H13.5v1.5z" fill="#fff"/>
+    </svg>
+  );
+}
