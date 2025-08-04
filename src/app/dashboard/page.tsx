69e6c66b75c08a592f6b568c8161fd86a0d2e4ed
Abinesh <108071239+knightempire@users.noreply.github.com>
2025-08-04 17:46:08 +0530

    Update dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,11 +1,22 @@
 
 "use client";
 import EmployeeTable from "@/components/EmployeeTable";
+import { useEffect } from "react";
 
 export default function Dashboard() {
+
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      window.location.href = "/";
+    } else {
+      console.log("Dashboard token:", token);
+    }
+  }, []);
+
   return (
     <div className="min-h-screen bg-white">
-    
       <main className="flex-1 p-8 pt-24 ml-64">
         <EmployeeTable />
       </main>
