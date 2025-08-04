a57b9688913458203cbb78359754539574c30b04
Abinesh <108071239+knightempire@users.noreply.github.com>
2025-08-04 19:41:05 +0530

    Add icons

---
+++ src/components/icons.tsx
@@ -0,0 +1,13 @@
+// /app/employees/components/icons.tsx
+"use client";
+
+export function FilterIcon() {
+  return (
+    <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg" className="mr-2">
+      <rect x="3" y="6" width="16" height="2.2" rx="1.1" fill="#6B7280" />
+      <rect x="5" y="10" width="12" height="2.2" rx="1.1" fill="#6B7280" />
+      <rect x="7" y="14" width="8" height="2.2" rx="1.1" fill="#6B7280" />
+      <rect x="9" y="18" width="4" height="2.2" rx="1.1" fill="#6B7280" />
+    </svg>
+  );
+}
