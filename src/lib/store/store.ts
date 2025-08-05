b69d1586ccc1863cdbbd34f2370d3959d78bb8c6
Abinesh <108071239+knightempire@users.noreply.github.com>
2025-08-05 09:05:53 +0530

    Update Redux store

--- src/lib/store/store.ts
+++ src/lib/store/store.ts
@@ -1,4 +1,3 @@
-// lib/store/store.ts
 
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './slices/authSlice';
@@ -6,17 +5,14 @@ import employeesReducer from './slices/employeesSlice';
 
 export const store = configureStore({
   reducer: {
-    // This is where you combine all your slices
+
     auth: authReducer,
     employees: employeesReducer,
   },
 });
 
-// These types are essential for using Redux with TypeScript
-// They allow you to get type hints for your state and dispatch functions
 
-// Infer the `RootState` type from the store itself
 export type RootState = ReturnType<typeof store.getState>;
 
-// Infer the `AppDispatch` type, which includes thunk middleware
+
 export type AppDispatch = typeof store.dispatch;
