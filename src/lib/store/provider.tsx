3f771b2e56f39754ba74ae2c8206830d68a9cd47
Abinesh <108071239+knightempire@users.noreply.github.com>
2025-08-05 09:05:53 +0530

    Update Redux provider

--- src/lib/store/provider.tsx
+++ src/lib/store/provider.tsx
@@ -1,12 +1,11 @@
-// lib/store/provider.tsx
+
 "use client";
 
 import React from 'react';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
-// This component wraps your entire application, providing it with the Redux store.
-// It must be a client component because it uses React's Context API via the <Provider>.
+
 export default function ReduxProvider({ children }: { children: React.ReactNode }) {
   return <Provider store={store}>{children}</Provider>;
 }
