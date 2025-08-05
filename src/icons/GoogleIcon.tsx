37957ea2fb739bfc9b1adb7d712647a4066e675e
Abinesh <108071239+knightempire@users.noreply.github.com>
2025-08-05 09:05:52 +0530

    Update GoogleIcon

--- src/icons/GoogleIcon.tsx
+++ src/icons/GoogleIcon.tsx
@@ -1,4 +1,4 @@
-// GoogleIcon.tsx
+
 export default function GoogleIcon(props: React.SVGProps<SVGSVGElement>) {
   return (
     <svg width={20} height={20} viewBox="0 0 20 20" fill="none" {...props}>
